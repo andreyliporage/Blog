ee07dc56b6e946639491c33cdd5cb41b22b6248b
andreyliporage <andreyliporage@gmail.com>
2023-08-31 09:17:20 -0300

    teste ef core

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using Blog.Data;
+using Blog.Models;
+
+using var context = new BlogDataContext();
+
+// var user = new User()
+// {
+//     Name = "Andrey Liporage",
+//     Email = "andreydev.io@gmail.com",
+//     Bio = "Desenvolvedor FullStack",
+//     Image = "google.com",
+//     PasswordHash = "a1b2c3e4f5g6",
+//     Slug = "andlmat"
+// };
+// context.Users.Add(user);
+// context.SaveChanges();
+
+var user = context.Users.FirstOrDefault();
+var post = new Post()
+{
+    Author = user,
+    Body = "Melhorando a perfomance utilizando .NET 6, SQL Server e EntityFramework Core",
+    Category = new Category{ Name = "Backend", Slug = "backend" },
+    Title = "Desafio Rinha Backend",
+    CreateDate = DateTime.Now.ToUniversalTime(),
+    Tags = null,
+    Summary = "Desafio",
+    Slug = "Back"
+};
+
+context.Posts.Add(post);
+context.SaveChanges();
